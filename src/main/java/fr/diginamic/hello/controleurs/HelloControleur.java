38e3a899bec25b614257f6ed4ddabab2b5e5ca96
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-11-24 12:34:49 +0100

    TP 2 terminé

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -1,5 +1,7 @@
 package fr.diginamic.hello.controleurs;
 
+import fr.diginamic.hello.controleurs.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
+    @Autowired
+    private HelloService service;
     @GetMapping
-    public String direHello(){
-        return "Hello"; } }
+    public String dire(){
+        return service.salutations(); }
+
+}
---
+++ src/main/java/fr/diginamic/hello/controleurs/services/Configuration.java
@@ -0,0 +1,10 @@
+package fr.diginamic.hello.controleurs.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Configuration {
+    public void configuration() {
+        System.out.println("je viens d'être créer");
+    }
+}
